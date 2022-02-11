cc0d2058c926df9b85f5524f25ba22119155af23
brotatotes <erichao2018@gmail.com>
2022-02-10 17:37:56 -0800

    add support for index.html

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
 app.use('/index', indexRouter);
+app.use('/index.html', indexRouter);
 
 app.post('/clicked', (req, res) => {
   const timestamp = (new Date()).toISOString();
